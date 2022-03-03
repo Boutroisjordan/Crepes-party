bef33e047516a869a5c0283405a4664af5f05ffb
jordanbtrs <jordanboutroispro@gmail.com>
2022-03-03 22:16:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-let convive;
-let newRecette = new Object;
+let convive; //nombres de personnes
 let listeDeCourse = {
     farine: 0,
     oeuf: 0,
@@ -24,58 +23,93 @@ let uniteProduct = {
     beurre: 500, //gramme
 };
 
+let noMoreCourse = false;
+let noMoreIngredients = false;
+let mesures = [];
+let tableauCourses = [];
 
+//Récupère le nombre d'inviter taper dans l'input text
 function getValue(){
     let input = document.getElementById("nombre-inviter").value;
     convive = input;
-    Crepier(convive);
+    Crepier(convive, recette);
 }
 
-// Crepier(convive);
+// Calcul les mesures pour réaliser une crêpes
+function Crepier(number, Object) {
+    mesures.splice(0, mesures.length);
+    for (var i = 0 in Object) {
+        mesures.push((number * Object[i]));
+    }
+    faireSesCourses(tableauCourses);
+};
 
-function Crepier(number) {
-    newRecette.farine = number * recette.farine; 
-    newRecette.oeuf = number * recette.oeuf;
-    newRecette.lait = number * recette.lait;
-    newRecette.sucre = number * recette.sucre;
-    newRecette.beurre = number * recette.beurre;
-
-    console.log(newRecette);
-    course();
-    return newRecette;
+// Calcule la quantité de courses à faire (en fonction des packaging magasin)
+function faireSesCourses() {
+    tableauCourses.splice(0, tableauCourses.length);
+    tableauCourses.push((Math.ceil(mesures[0] / uniteProduct.farine)));
+    tableauCourses.push((Math.ceil(mesures[1] / uniteProduct.oeuf)));
+    tableauCourses.push((Math.ceil(mesures[2] / uniteProduct.lait)));
+    tableauCourses.push((Math.ceil(mesures[3] / uniteProduct.sucre)));
+    tableauCourses.push((Math.ceil(mesures[4] / uniteProduct.beurre)));
+    goToHtml(mesures, tableauCourses);
     
 };
 
-function course() {
-    
-    listeDeCourse.farine = Math.ceil(newRecette.farine / uniteProduct.farine);
-    listeDeCourse.oeuf = Math.ceil(newRecette.oeuf / uniteProduct.oeuf);
-    listeDeCourse.lait = Math.ceil(newRecette.lait / uniteProduct.lait);
-    listeDeCourse.sucre = Math.ceil(newRecette.sucre / uniteProduct.sucre);
-    listeDeCourse.beurre = Math.ceil(newRecette.beurre / uniteProduct.beurre);
+// Envoie les mesusres et les courses dans le HTML
+function goToHtml(array, array2) {
+    //Ingrédients
+    let ingredients = ["farine", "oeuf", "lait", "sucre", "beurre"];
+    let ulCourses =  document.getElementById('courses');
+    let ulRecette = document.getElementById('ingredients');
+    if (!noMoreIngredients) {
+        for (let index = 0; index < ingredients.length; index++) {
+            let link = document.createElement("li");
+            ulRecette.appendChild(link);
+            link.setAttribute(`id`, `${ingredients[index]}-recette`);
+            link.innerHTML = `<img src="img/${ingredients[index]}.png" alt="">` + array[index];
+        }
+        noMoreIngredients = true;
+    } else if (noMoreIngredients) {
 
-    console.log(listeDeCourse);
+        for (let index = 0; index < ingredients.length; index++) {
+            let insert = document.getElementById(`${ingredients[index]}-recette`);
+            insert.innerHTML = `<img src="img/${ingredients[index]}.png" alt="">` + array[index];
+        }
+    };
 
-    goToHtml();
-    return listeDeCourse;
-    
-};
+    // COURSES
 
-function goToHtml() {
-    document.getElementById("farine-recette").innerText = newRecette.farine + "g";
-    document.getElementById("oeuf-recette").innerText =  newRecette.oeuf ;
-    document.getElementById("lait-recette").innerText =  newRecette.lait + "L";
-    document.getElementById("sucre-recette").innerText = newRecette.sucre + "g";
-    document.getElementById("beurre-recette").innerText = newRecette.beurre + "g";
+    if (!noMoreCourse) {
+        for (let index = 0; index < ingredients.length; index++) {
+            let link = document.createElement("li");
+            ulCourses.appendChild(link);
+            link.setAttribute(`id`, `${ingredients[index]}-courses`);
+            link.innerHTML = `<img src="img/${ingredients[index]}.png" alt="">` + array2[index];
+        }
+        noMoreCourse = true;
 
-    // courses
+    } else if (noMoreCourse) {
+        for (let index = 0; index < ingredients.length; index++) {
+            let insert = document.getElementById(`${ingredients[index]}-courses`);
+            insert.innerHTML = `<img src="img/${ingredients[index]}.png" alt="">` + array2[index];
+        }
+    }
 
-    document.getElementById("farine-courses").innerText = listeDeCourse.farine + " paquets";
-    document.getElementById("oeuf-courses").innerText = listeDeCourse.oeuf ;
-    document.getElementById("lait-courses").innerText = listeDeCourse.lait + " brique";
-    document.getElementById("sucre-courses").innerText =  listeDeCourse.sucre + " paquets";
-    document.getElementById("beurre-courses").innerText =  listeDeCourse.beurre + " plaquette ";
+
+
+
+    // document.getElementById("farine-courses").innerHTML = `<img src="img/farine.png" alt="farine">` + listeDeCourse.farine + " paquets";
+    // document.getElementById("oeuf-courses").innerHTML = `<img src="img/oeuf-frit.png" alt="oeuf">` + listeDeCourse.oeuf ;
+    // document.getElementById("lait-courses").innerHTML = `<img src="img/carton-de-lait.png" alt="lait">` + listeDeCourse.lait + " brique";
+    // document.getElementById("sucre-courses").innerHTML =  `<img src="img/sucre.png" alt="sucre">` + listeDeCourse.sucre + " paquets";
+    // document.getElementById("beurre-courses").innerHTML =  `<img src="img/beurre.png" alt="beurre">` + listeDeCourse.beurre + " plaquette ";
 }
+
+
+
+
+
 
 
 
